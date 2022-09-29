2bc372a5b5531b28ce5af4976283e04aaee100ef
Nick <nicolas.cinera@gmail.com>
2022-09-29 11:45:21 -0400

    Imports

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,16 +1,18 @@
 import {useState} from 'react';
+import femaleProfile from './images/femaleProfile.jpg';
+import maleProfile from './images/maleProfile.jpg';
 
 const Employees = () => {
 
     const [employees, setEmployees] = useState([
-        {id: 1, name: 'John', age: 20, position: 'Developer'},
-        {id: 2, name: 'Sara', age: 22, position: 'Designer'},
-        {id: 3, name: 'Bob', age: 24, position: 'The Boss'},
-        {id: 4, name: 'Peter', age: 26, position: 'Intern'},
-        {id: 5, name: 'Susan', age: 28, position: 'Accountant'},
-        {id: 6, name: 'Anna', age: 30, position: 'CEO'},
-        {id: 7, name: 'John', age: 20, position: 'Developer'},
-        {id: 8, name: 'Sara', age: 22, position: 'Designer'}
+        {id: 1, name: 'John', age: 20, position: 'Developer', image: maleProfile},
+        {id: 2, name: 'Sara', age: 22, position: 'Designer', image: femaleProfile},
+        {id: 3, name: 'Bob', age: 24, position: 'The Boss', image: maleProfile},
+        {id: 4, name: 'Peter', age: 26, position: 'Intern', image: maleProfile},
+        {id: 5, name: 'Susan', age: 28, position: 'Accountant', image: femaleProfile},
+        {id: 6, name: 'Anna', age: 30, position: 'CEO', image: femaleProfile},
+        {id: 7, name: 'John', age: 20, position: 'Developer', image: maleProfile},
+        {id: 8, name: 'Sara', age: 22, position: 'Designer', image: femaleProfile},
     ]);
 
     return(
@@ -19,7 +21,7 @@ const Employees = () => {
                 <h2 className="mt-4 p5 bg-primary text-white rounded">Our Employees</h2>
                 <div className="row">
                     {employees.map((employee) => {
-                        const {id, name, age, position} = employee;
+                        const {id, name, age, position, image} = employee;
                         return(
                             <div className="col-md-4" key={id}>
                                 <div className="card mb-4">
@@ -29,6 +31,7 @@ const Employees = () => {
 
                                         <p className="card-text">{age} years old</p>
                                     </div>
+                                    <img src={image} className="card-img-bottom" alt="employee" />
                                 </div>
                             </div>
                         )
